aae5185d3ef55fc233e4479a9767a9e6b98b1d10
Kritika Bhandari <spiderbasnet140@gmail.com>
2025-06-19 00:56:43 +0545

    size firstnode lstnode add

--- LinkList.js
+++ LinkList.js
@@ -18,28 +18,57 @@ class LinkList{
      printData(){
          let x = this.head;
          while (x !== null) {
-             console.log(x.value)
+             console.log("->" , x.value)
                 x = x.next;
             }
         
      }
+     insertAfter(value,toInsert){
+        let x = this.head;
+
+        while(x !== null){
+            if(value === x.value){
+                let t = new Node(toInsert,x.next);
+                x.next = t;
+            }
+            x = x.next;
+        }
+        
+     }
+     
      removeData(data){
         let todlt = this.head;
         let newList = this.head.next;
 
         while(newList !== null){
-            // console.log("this is old list value ", todlt.value)
-            console.log("this is NEWWW list value ", newList.value)
-
             if(newList.value === data){
                 todlt.next = newList.next;
             }
-
             todlt = newList;
             newList = newList.next;
         }
 
      }
+
+     size(){
+        let x = this.head;
+        let current = this.head.next;
+        let o = 0;
+        while(current !== null){
+            
+            x = x.next;
+            current = current.next
+            o++;
+        }
+        console.log("Last Node: ",x.value)
+        console.log("total size of the Linked List: ", o)
+     }
+
+     firstNode(){
+        console.log("This is the First Node", this.head.value)
+     }
+
+
 }
 
 let newList = new LinkList();
@@ -48,6 +77,8 @@ newList.append("iphone")
 newList.append("fan")
 newList.append("table")
 newList.append("dog")
-newList.removeData("fan");
-
+// newList.removeData("fan");
+newList.insertAfter("fan","Prajwal")
+newList.size()
+newList.firstNode()
 newList.printData()
