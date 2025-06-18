3e1b3e85760b5eaaad3e999126acdad89b8baaef
Kritika Bhandari <spiderbasnet140@gmail.com>
2025-06-19 00:29:07 +0545

    some chng

--- LinkList.js
+++ LinkList.js
@@ -1,33 +1,53 @@
-
 class Node{
-    constructor(data,nextNode){
-        this.data = data;
-        this.nextNode = nextNode;
+    constructor(value,next){
+        this.value = value;
+        this.next = next;
     }
 }
-    
-    class LinkList{
-        constructor(){
-            this.head = null;
-        }
-    
-        addFirst(data){
-            this.head = new Node(data,this.head);
-        }
-        printList(){
-            let x = this.head;
-            while (x != null) {
-                console.log(x.data)
-                x = x.nextNode
+
+class LinkList{
+     constructor(){
+        this.head = null;
+     }
+
+     append(data){
+        let newNode = new Node(data,this.head);
+        this.head = newNode
+     }
+
+     printData(){
+         let x = this.head;
+         while (x !== null) {
+             console.log(x.value)
+                x = x.next;
             }
-        }
         
-    }
+     }
+     removeData(data){
+        let todlt = this.head;
+        let newList = this.head.next;
 
+        while(newList !== null){
+            // console.log("this is old list value ", todlt.value)
+            console.log("this is NEWWW list value ", newList.value)
 
-    let myLink = new LinkList();
-    myLink.addFirst("cat")
-    myLink.addFirst("dog")
-    myLink.addFirst("cowog")
+            if(newList.value === data){
+                todlt.next = newList.next;
+            }
 
-    myLink.printList()
+            todlt = newList;
+            newList = newList.next;
+        }
+
+     }
+}
+
+let newList = new LinkList();
+
+newList.append("iphone")
+newList.append("fan")
+newList.append("table")
+newList.append("dog")
+newList.removeData("fan");
+
+newList.printData()
